202ec96e0a3d4e71da6fcfb8aa1595cdd73285cf
Joel <joel.garplind@ffcg.se>
2021-09-24 19:26:20 +0200

    Less ugly perils

--- src/features/perils/Peril.tsx
+++ src/features/perils/Peril.tsx
@@ -1,3 +1,6 @@
+import styled from "@emotion/styled";
+import { colorsV3 } from "@hedviginsurance/brand";
+
 export interface IPeril {
   title: string;
   description: string;
@@ -7,6 +10,25 @@ export interface IPeril {
   icon: { variants: { light: { svgUrl: string } } };
 }
 
+const Button = styled.button`
+  padding: 0;
+  border: none;
+  font: inherit;
+  color: inherit;
+  background-color: transparent;
+  cursor: pointer;
+  height: 160px;
+  border: 1px solid ${colorsV3.gray900};
+  border-radius: 0.375rem;
+`;
+
 export const Peril = ({ title, icon }: Pick<IPeril, "title" | "icon">) => {
-  return <button>{`${title}`}</button>;
+  return (
+    <Button>
+      {`${title}`}
+      <svg width="90" height="90">
+        <image xlinkHref={icon.variants.light.svgUrl} width="90" height="90" />
+      </svg>
+    </Button>
+  );
 };
