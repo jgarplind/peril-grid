481bafa954c9fbb39897962a8ec38a3b683f677f
Joel <joel.garplind@ffcg.se>
2021-09-24 18:38:09 +0200

    Improved FluentGrid API and render result sligthly

--- src/components/FluidGrid.tsx
+++ src/components/FluidGrid.tsx
@@ -1,3 +1,9 @@
-export const FluidGrid = () => {
-  return <div></div>;
+export const FluidGrid = ({ items }: { items: JSX.Element[] }) => {
+  return (
+    <div>
+      {items.map((item) => (
+        <div>{item}</div>
+      ))}
+    </div>
+  );
 };
