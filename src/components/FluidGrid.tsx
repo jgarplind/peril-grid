ae1fc06ef4555bb1cf1b154a2458fe946afeef39
Joel <joel.garplind@ffcg.se>
2021-09-24 19:26:04 +0200

    Made FluidGrid actually fluid

--- src/components/FluidGrid.tsx
+++ src/components/FluidGrid.tsx
@@ -1,9 +1,19 @@
+import styled from "@emotion/styled";
+
+const Grid = styled.div`
+  display: grid;
+  gap: 16px;
+  padding: 16px;
+  align-items: center;
+  grid-template-columns: repeat(auto-fill, minmax(150px, 1fr));
+`;
+
 export const FluidGrid = ({ items }: { items: JSX.Element[] }) => {
   return (
-    <div>
+    <Grid>
       {items.map((item) => (
-        <div>{item}</div>
+        <>{item}</>
       ))}
-    </div>
+    </Grid>
   );
 };
